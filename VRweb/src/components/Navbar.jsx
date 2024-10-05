1316b87a49723e3eda1c73c21d5c3a3e605a36b5
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-10-05 22:32:01 +0530

    Nav bar sketch

--- VRweb/src/components/Navbar.jsx
+++ VRweb/src/components/Navbar.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
+      <div className="container px-4 mx-auto relative lg:text-sm">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center flex-shrink-0">
+          </div>
+        </div> 
+      </div>
+    </nav>
   )
 }
 
