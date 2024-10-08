f414e56329c54f9dd8f3c643d29872e8fc78cb77
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-10-08 15:34:16 +0530

    Update names & videos of page

--- VRweb/src/components/HeroSection.jsx
+++ VRweb/src/components/HeroSection.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import video1 from '../assets/video1.mp4';
-import video2 from '../assets/video2.mp4';
+import video3 from '../assets/video3.mp4';
+import video4 from '../assets/video4.mp4';
 
 const HeroSection = () => {
   return (
@@ -8,7 +8,7 @@ const HeroSection = () => {
     <div className="flex flex-col items-center mt-6 lg:mt-20">
 
         <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
-            VirtualR build tools 
+            AIForge build tools 
             <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">
                 {" "}
                 for developers
@@ -16,9 +16,7 @@ const HeroSection = () => {
         </h1>
 
         <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
-            Empower your creativity and bring your VR app ideas to life with our
-            intuitive development tools. Get started today and turn your imagination
-            into immersive reality!
+          Unlock the potential of artificial intelligence in your apps with AIForge. Our developer-friendly tools allow you to integrate advanced AI models and machine learning algorithms without the need for deep technical expertise. Start building smarter applications today, powered by AI!
         </p>
         
         <div className='flex justify-center my-10'>
@@ -34,12 +32,12 @@ const HeroSection = () => {
         <div className='flex justify-center mt-10'>
 
           <video autoPlay loop muted className='rounded-lg w-1/2 border border-orange-700 shadow-sm shadow-orange-400 mx-2 my-4'>
-            <source src={video1} type='video/mp4' />
+            <source src={video3} type='video/mp4' />
             Your browser does not support the video tag.
           </video>
 
           <video autoPlay loop muted className='rounded-lg w-1/2 border border-orange-700 shadow-sm shadow-orange-400 mx-2 my-4'>
-            <source src={video2} type='video/mp4' />
+            <source src={video4} type='video/mp4' />
             Your browser does not support the video tag.
           </video>
 
