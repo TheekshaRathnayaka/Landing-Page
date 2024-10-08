825a95a900b7889649067a57c4edcf4097dbb2e6
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-10-08 11:37:13 +0530

    Complete designing HeroSection

--- VRweb/src/components/HeroSection.jsx
+++ VRweb/src/components/HeroSection.jsx
@@ -20,8 +20,33 @@ const HeroSection = () => {
             intuitive development tools. Get started today and turn your imagination
             into immersive reality!
         </p>
+        
+        <div className='flex justify-center my-10'>
+          <a href='#' className='bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md'>
+            Start for Free
+          </a>
+
+          <a href='#' className='py-3 px-4 mx-3 rounded-md border'>
+            Documentation
+          </a>
+        </div>
+
+        <div className='flex justify-center mt-10'>
+
+          <video autoPlay loop muted className='rounded-lg w-1/2 border border-orange-700 shadow-sm shadow-orange-400 mx-2 my-4'>
+            <source src={video1} type='video/mp4' />
+            Your browser does not support the video tag.
+          </video>
+
+          <video autoPlay loop muted className='rounded-lg w-1/2 border border-orange-700 shadow-sm shadow-orange-400 mx-2 my-4'>
+            <source src={video2} type='video/mp4' />
+            Your browser does not support the video tag.
+          </video>
+
+        </div>
+
     </div>
-  )
-}
+  );
+};
 
 export default HeroSection;
