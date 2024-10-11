47c3dd0889784fa5d210053d2b603c4aaf8c50a1
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-10-11 20:17:11 +0530

    Finalize testimonial section

--- VRweb/src/components/Testimonials.jsx
+++ VRweb/src/components/Testimonials.jsx
@@ -19,12 +19,24 @@ const Testimonials = () => {
                         <p>{testimonial.text}</p>
                         
                         <div className='flex mt-8 items-start'>
+
                             <img className='w-12 h-12 mr-6 rounded-full border border-neutral-300' 
                             src={testimonial.image} alt='' />
+
+                            <div>
+                                <h6>{testimonial.user}</h6>
+                                <span className='text-sm font-normal italic text-neutral-600'>
+                                    {testimonial.company}
+                                </span>
+                            </div>
+
                         </div>
+
                     </div>
+
                 </div>
             ))}
+
         </div>
       
     </div>
@@ -32,3 +44,4 @@ const Testimonials = () => {
 }
 
 export default Testimonials
+
