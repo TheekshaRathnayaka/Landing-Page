9fef6ff465b77437f8043b8fdb6977e11f72636d
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-10-11 19:23:04 +0530

    List pricing features

--- VRweb/src/components/Pricing.jsx
+++ VRweb/src/components/Pricing.jsx
@@ -11,9 +11,12 @@ const Pricing = () => {
         </h2>
 
         <div className='flex flex-wrap'>
+
             {pricingOptions.map((option, index) => (
                 <div key={index} className='w-full sm:w-1/2 lg:w-1/3 p-2'>
+
                     <div className='p-10 border border-neutral-700 rounded-xl'>
+
                         <p className='text-4xl mb-8'>
                             {option.title}
                             {option.title === 'Pro' && (
@@ -22,7 +25,25 @@ const Pricing = () => {
                                 </span>
                             )}
                         </p>
+
+                        <p className='mb-8'>
+                            <span className='text-5xl mt-6 mr-2'>
+                                {option.price}
+                            </span>
+                            <span className='text-neutral-400 tracking-tight'>/Month</span>
+                        </p>
+
+                        <ul>
+                            {option.features.map((feature, index) => (
+                                <li key={index} className='mt-8 flex items-center'>
+                                    <CheckCircle2 />
+                                    <span className='ml-2'>{feature}</span>
+                                </li>
+                            ))}
+                        </ul>
+                        
                     </div>
+
                 </div>
              ))
             }
