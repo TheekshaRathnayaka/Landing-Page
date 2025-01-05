03c0e8b93876b583e6ad42409fa6ceabd1ff0d31
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2025-01-05 22:30:37 +0530

    Link navbar to HeroSection

--- VRweb/src/components/Navbar.jsx
+++ VRweb/src/components/Navbar.jsx
@@ -19,8 +19,10 @@ const Navbar = () => {
         <div className="flex justify-between items-center">
 
           <div className="flex items-center flex-shrink-0">
-            <img className="h-10 w-10 mr-2" src={logo} alt="Logo" />
-            <span className="text-xl tracking-tight">AIForge</span>
+            <a href='#home'>
+              <img className="h-10 w-10 mr-2" src={logo} alt="Logo" />
+              <span className="text-xl tracking-tight">AIForge</span>
+            </a>
           </div>
 
           <ul className="hidden lg:flex ml-14 space-x-12">
